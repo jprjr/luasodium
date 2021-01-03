c65921b0cb7dba277cd3a1326e03ad39c12da392
John Regan <john@jrjrtech.com>
2021-01-03 18:47:15 -0500

    ffi-only: forgot to include latest submodules

--- ffi/luasodium.lua
+++ ffi/luasodium.lua
@@ -1,31 +1,26 @@
 local M = {}
 
-local crypto_auth        = require'luasodium.crypto_auth'
-local crypto_box         = require'luasodium.crypto_box'
-local crypto_hash         = require'luasodium.crypto_hash'
-local crypto_secretbox   = require'luasodium.crypto_secretbox'
-local crypto_sign        = require'luasodium.crypto_sign'
-local crypto_scalarmult  = require'luasodium.crypto_scalarmult'
-local randombytes        = require'luasodium.randombytes'
-local utils              = require'luasodium.utils'
-local version            = require'luasodium.version'
-
 local modules = {
-  crypto_auth,
-  crypto_box,
-  crypto_hash,
-  crypto_scalarmult,
-  crypto_secretbox,
-  crypto_sign,
-  randombytes,
-  utils,
-  version,
+  'crypto_auth',
+  'crypto_box',
+  'crypto_hash',
+  'crypto_onetimeauth',
+  'crypto_scalarmult',
+  'crypto_secretbox',
+  'crypto_sign',
+  'crypto_stream',
+  'crypto_verify',
+  'randombytes',
+  'utils',
+  'version',
 }
 
-for _,mod in ipairs(modules) do
+for _,m in ipairs(modules) do
+  local mod = require('luasodium.' .. m)
   for k,v in pairs(mod) do
     M[k] = v
   end
 end
+
 
 return M
