8af03f5c7ad54dd363ffc0ea956db212527b1524
John Regan <john@jrjrtech.com>
2021-01-08 13:16:24 -0500

    add testing for windows

---
+++ test/test.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+int main(int argc, const char *argv[]) {
+    int r = 0;
+    lua_State *L = NULL;
+    if(argc < 2) {
+        return 1;
+    }
+
+    L = luaL_newstate();
+    if(L == NULL) abort();
+    luaL_openlibs(L);
+
+    r = luaL_dofile(L,argv[1]);
+
+    lua_close(L);
+
+    return r;
+}
