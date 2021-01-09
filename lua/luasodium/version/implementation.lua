d377f4e8b31c706be78ed54a1a25c639ed5d3aee
John Regan <john@jrjrtech.com>
2021-01-09 14:43:16 -0500

    start version 1.1.0

--- lua/luasodium/version/implementation.lua
+++ lua/luasodium/version/implementation.lua
@@ -1,6 +1,6 @@
 local _VERSION_MAJOR = 1
-local _VERSION_MINOR = 0
-local _VERSION_PATCH = 5
+local _VERSION_MINOR = 1
+local _VERSION_PATCH = 0
 
 local _VERSION = string.format('%d.%d.%d',
   _VERSION_MAJOR,
