1ff0125f2f198fc9ccb80df417f1c177bd9628d9
John Regan <john@jrjrtech.com>
2021-01-10 20:50:19 -0500

    crypto_auth: remove copy/paste/search/replace functions

--- c/luasodium/crypto_auth/core.c
+++ c/luasodium/crypto_auth/core.c
@@ -1,37 +1,70 @@
 #include "../luasodium-c.h"
-#include "../internals/ls_lua_set_functions.h"
 #include "../internals/ls_lua_set_constants.h"
 #include "constants.h"
 
+typedef int (*ls_crypto_auth_ptr)(
+  unsigned char *,
+  const unsigned char *,
+  unsigned long long,
+  const unsigned char *);
+
+typedef int (*ls_crypto_auth_verify_ptr)(
+  const unsigned char *m,
+  const unsigned char *,
+  unsigned long long,
+  const unsigned char *);
+
+typedef void (*ls_crypto_auth_keygen_ptr)(
+  unsigned char *);
+
 static int
 ls_crypto_auth(lua_State *L) {
-    unsigned char out[crypto_auth_BYTES];
+    unsigned char *out = NULL;
     const unsigned char *in = NULL;
     const unsigned char *k = NULL;
 
     size_t inlen = 0;
     size_t klen = 0;
 
+    const char *fname = NULL;
+    ls_crypto_auth_ptr f = NULL;
+    size_t BYTES = 0;
+    size_t KEYBYTES = 0;
+
     if(lua_isnoneornil(L,2)) {
         lua_pushliteral(L,"requires 2 parameters");
         return lua_error(L);
     }
 
+    fname = lua_tostring(L,lua_upvalueindex(1));
+    f     = (ls_crypto_auth_ptr)lua_touserdata(L,lua_upvalueindex(2));
+    BYTES = lua_tointeger(L,lua_upvalueindex(3));
+    KEYBYTES = lua_tointeger(L,lua_upvalueindex(4));
+
     in = (const unsigned char *)lua_tolstring(L,1,&inlen);
     k =  (const unsigned char *)lua_tolstring(L,2,&klen);
 
-    if(klen != crypto_auth_KEYBYTES) {
-        return luaL_error(L,"wrong key size, expected: %d", crypto_auth_KEYBYTES);
+    if(klen != KEYBYTES) {
+        return luaL_error(L,"wrong key size, expected: %d", KEYBYTES);
     }
 
+    out = (unsigned char *)lua_newuserdata(L,BYTES);
+
     /* LCOV_EXCL_START */
-    if(crypto_auth(out,in,inlen,k) == -1) {
-        return luaL_error(L,"crypto_auth error");
+    if(out == NULL) {
+        return luaL_error(L,"out of memory");
+    }
+    /* LCOV_EXCL_STOP */
+    lua_pop(L,1);
+
+    /* LCOV_EXCL_START */
+    if(f(out,in,inlen,k) == -1) {
+        return luaL_error(L,"%s error",fname);
     }
     /* LCOV_EXCL_STOP */
 
-    lua_pushlstring(L,(const char *)out,crypto_auth_BYTES);
-    sodium_memzero(out,crypto_auth_BYTES);
+    lua_pushlstring(L,(const char *)out,BYTES);
+    sodium_memzero(out,BYTES);
     return 1;
 }
 
@@ -45,267 +78,82 @@ ls_crypto_auth_verify(lua_State *L) {
     size_t inlen = 0;
     size_t klen = 0;
 
+    ls_crypto_auth_verify_ptr f = NULL;
+    size_t BYTES = 0;
+    size_t KEYBYTES = 0;
+
     if(lua_isnoneornil(L,3)) {
         lua_pushliteral(L,"requires 3 parameters");
         return lua_error(L);
     }
 
+    f     = (ls_crypto_auth_verify_ptr)lua_touserdata(L,lua_upvalueindex(1));
+    BYTES = lua_tointeger(L,lua_upvalueindex(2));
+    KEYBYTES = lua_tointeger(L,lua_upvalueindex(3));
+
     h =  (const unsigned char *)lua_tolstring(L,1,&hlen);
     in = (const unsigned char *)lua_tolstring(L,2,&inlen);
     k =  (const unsigned char *)lua_tolstring(L,3,&klen);
 
-    if(hlen != crypto_auth_BYTES) {
-        return luaL_error(L,"wrong tag size, expected: %d", crypto_auth_BYTES);
+    if(hlen != BYTES) {
+        return luaL_error(L,"wrong tag size, expected: %d", BYTES);
     }
 
-    if(klen != crypto_auth_KEYBYTES) {
-        return luaL_error(L,"wrong key size, expected: %d", crypto_auth_KEYBYTES);
+    if(klen != KEYBYTES) {
+        return luaL_error(L,"wrong key size, expected: %d", KEYBYTES);
     }
 
-    lua_pushboolean(L,crypto_auth_verify(h,in,inlen,k) == 0);
+    lua_pushboolean(L,f(h,in,inlen,k) == 0);
     return 1;
 }
 
 static int
 ls_crypto_auth_keygen(lua_State *L) {
-    unsigned char k[crypto_auth_KEYBYTES];
-    crypto_auth_keygen(k);
-    lua_pushlstring(L,(const char *)k,crypto_auth_KEYBYTES);
-    sodium_memzero(k,crypto_auth_KEYBYTES);
-    return 1;
-}
+    unsigned char *k = NULL;
 
-static int
-ls_crypto_auth_hmacsha256(lua_State *L) {
-    unsigned char out[crypto_auth_hmacsha256_BYTES];
-    const unsigned char *in = NULL;
-    const unsigned char *k = NULL;
+    ls_crypto_auth_keygen_ptr f = NULL;
+    size_t KEYBYTES = 0;
 
-    size_t inlen = 0;
-    size_t klen = 0;
+    f     = (ls_crypto_auth_keygen_ptr)lua_touserdata(L,lua_upvalueindex(1));
+    KEYBYTES = lua_tointeger(L,lua_upvalueindex(2));
 
-    if(lua_isnoneornil(L,2)) {
-        lua_pushliteral(L,"requires 2 parameters");
-        return lua_error(L);
-    }
-
-    in = (const unsigned char *)lua_tolstring(L,1,&inlen);
-    k =  (const unsigned char *)lua_tolstring(L,2,&klen);
-
-    if(klen != crypto_auth_hmacsha256_KEYBYTES) {
-        return luaL_error(L,"wrong key size, expected: %d", crypto_auth_hmacsha256_KEYBYTES);
-    }
-
+    k = (unsigned char *)lua_newuserdata(L,KEYBYTES);
+    
     /* LCOV_EXCL_START */
-    if(crypto_auth_hmacsha256(out,in,inlen,k) == -1) {
-        return luaL_error(L,"crypto_auth_hmacsha256 error");
+    if(k == NULL) {
+        return luaL_error(L,"out of memory");
     }
     /* LCOV_EXCL_STOP */
+    lua_pop(L,1);
 
-    lua_pushlstring(L,(const char *)out,crypto_auth_hmacsha256_BYTES);
-    sodium_memzero(out,crypto_auth_hmacsha256_BYTES);
+    f(k);
+
+    lua_pushlstring(L,(const char *)k,KEYBYTES);
+    sodium_memzero(k,KEYBYTES);
     return 1;
 }
 
-static int
-ls_crypto_auth_hmacsha256_verify(lua_State *L) {
-    const unsigned char *h = NULL;
-    const unsigned char *in = NULL;
-    const unsigned char *k = NULL;
+#define LS_PUSH_CRYPTO_AUTH(x, y) \
+  lua_pushliteral(L, #x ); \
+  lua_pushlightuserdata(L, x); \
+  lua_pushinteger(L, x ## _BYTES); \
+  lua_pushinteger(L, x ## _KEYBYTES); \
+  lua_pushcclosure(L, ls_ ## y, 4); \
+  lua_setfield(L,-2, #x);
 
-    size_t hlen = 0;
-    size_t inlen = 0;
-    size_t klen = 0;
+#define LS_PUSH_CRYPTO_AUTH_VERIFY(x, y) \
+  lua_pushlightuserdata(L, x ## _verify); \
+  lua_pushinteger(L, x ## _BYTES); \
+  lua_pushinteger(L, x ## _KEYBYTES); \
+  lua_pushcclosure(L,ls_ ## y ## _verify, 3); \
+  lua_setfield(L,-2, #x "_verify");
 
-    if(lua_isnoneornil(L,3)) {
-        lua_pushliteral(L,"requires 3 parameters");
-        return lua_error(L);
-    }
+#define LS_PUSH_CRYPTO_AUTH_KEYGEN(x, y) \
+  lua_pushlightuserdata(L, x ## _keygen); \
+  lua_pushinteger(L, x ## _KEYBYTES); \
+  lua_pushcclosure(L,ls_ ## y ## _keygen, 2); \
+  lua_setfield(L,-2, #x "_keygen");
 
-    h =  (const unsigned char *)lua_tolstring(L,1,&hlen);
-    in = (const unsigned char *)lua_tolstring(L,2,&inlen);
-    k =  (const unsigned char *)lua_tolstring(L,3,&klen);
-
-    if(hlen != crypto_auth_hmacsha256_BYTES) {
-        return luaL_error(L,"wrong tag size, expected: %d", crypto_auth_hmacsha256_BYTES);
-    }
-
-    if(klen != crypto_auth_hmacsha256_KEYBYTES) {
-        return luaL_error(L,"wrong key size, expected: %d", crypto_auth_hmacsha256_KEYBYTES);
-    }
-
-    lua_pushboolean(L,crypto_auth_hmacsha256_verify(h,in,inlen,k) == 0);
-    return 1;
-}
-
-static int
-ls_crypto_auth_hmacsha256_keygen(lua_State *L) {
-    unsigned char k[crypto_auth_hmacsha256_KEYBYTES];
-    crypto_auth_hmacsha256_keygen(k);
-    lua_pushlstring(L,(const char *)k,crypto_auth_hmacsha256_KEYBYTES);
-    sodium_memzero(k,crypto_auth_hmacsha256_KEYBYTES);
-    return 1;
-}
-
-static int
-ls_crypto_auth_hmacsha512256(lua_State *L) {
-    unsigned char out[crypto_auth_hmacsha512256_BYTES];
-    const unsigned char *in = NULL;
-    const unsigned char *k = NULL;
-
-    size_t inlen = 0;
-    size_t klen = 0;
-
-    if(lua_isnoneornil(L,2)) {
-        lua_pushliteral(L,"requires 2 parameters");
-        return lua_error(L);
-    }
-
-    in = (const unsigned char *)lua_tolstring(L,1,&inlen);
-    k =  (const unsigned char *)lua_tolstring(L,2,&klen);
-
-    if(klen != crypto_auth_hmacsha512256_KEYBYTES) {
-        return luaL_error(L,"wrong key size, expected: %d", crypto_auth_hmacsha512256_KEYBYTES);
-    }
-
-    /* LCOV_EXCL_START */
-    if(crypto_auth_hmacsha512256(out,in,inlen,k) == -1) {
-        return luaL_error(L,"crypto_auth_hmacsha512256 error");
-    }
-    /* LCOV_EXCL_STOP */
-
-    lua_pushlstring(L,(const char *)out,crypto_auth_hmacsha512256_BYTES);
-    sodium_memzero(out,crypto_auth_hmacsha512256_BYTES);
-    return 1;
-}
-
-static int
-ls_crypto_auth_hmacsha512256_verify(lua_State *L) {
-    const unsigned char *h = NULL;
-    const unsigned char *in = NULL;
-    const unsigned char *k = NULL;
-
-    size_t hlen = 0;
-    size_t inlen = 0;
-    size_t klen = 0;
-
-    if(lua_isnoneornil(L,3)) {
-        lua_pushliteral(L,"requires 3 parameters");
-        return lua_error(L);
-    }
-
-    h =  (const unsigned char *)lua_tolstring(L,1,&hlen);
-    in = (const unsigned char *)lua_tolstring(L,2,&inlen);
-    k =  (const unsigned char *)lua_tolstring(L,3,&klen);
-
-    if(hlen != crypto_auth_hmacsha512256_BYTES) {
-        return luaL_error(L,"wrong tag size, expected: %d", crypto_auth_hmacsha512256_BYTES);
-    }
-
-    if(klen != crypto_auth_hmacsha512256_KEYBYTES) {
-        return luaL_error(L,"wrong key size, expected: %d", crypto_auth_hmacsha512256_KEYBYTES);
-    }
-
-    lua_pushboolean(L,crypto_auth_hmacsha512256_verify(h,in,inlen,k) == 0);
-    return 1;
-}
-
-static int
-ls_crypto_auth_hmacsha512256_keygen(lua_State *L) {
-    unsigned char k[crypto_auth_hmacsha512256_KEYBYTES];
-    crypto_auth_hmacsha512256_keygen(k);
-    lua_pushlstring(L,(const char *)k,crypto_auth_hmacsha512256_KEYBYTES);
-    sodium_memzero(k,crypto_auth_hmacsha512256_KEYBYTES);
-    return 1;
-}
-
-static int
-ls_crypto_auth_hmacsha512(lua_State *L) {
-    unsigned char out[crypto_auth_hmacsha512_BYTES];
-    const unsigned char *in = NULL;
-    const unsigned char *k = NULL;
-
-    size_t inlen = 0;
-    size_t klen = 0;
-
-    if(lua_isnoneornil(L,2)) {
-        lua_pushliteral(L,"requires 2 parameters");
-        return lua_error(L);
-    }
-
-    in = (const unsigned char *)lua_tolstring(L,1,&inlen);
-    k =  (const unsigned char *)lua_tolstring(L,2,&klen);
-
-    if(klen != crypto_auth_hmacsha512_KEYBYTES) {
-        return luaL_error(L,"wrong key size, expected: %d", crypto_auth_hmacsha512_KEYBYTES);
-    }
-
-    /* LCOV_EXCL_START */
-    if(crypto_auth_hmacsha512(out,in,inlen,k) == -1) {
-        return luaL_error(L,"crypto_auth_hmacsha512 error");
-    }
-    /* LCOV_EXCL_STOP */
-
-    lua_pushlstring(L,(const char *)out,crypto_auth_hmacsha512_BYTES);
-    sodium_memzero(out,crypto_auth_hmacsha512_BYTES);
-    return 1;
-}
-
-static int
-ls_crypto_auth_hmacsha512_verify(lua_State *L) {
-    const unsigned char *h = NULL;
-    const unsigned char *in = NULL;
-    const unsigned char *k = NULL;
-
-    size_t hlen = 0;
-    size_t inlen = 0;
-    size_t klen = 0;
-
-    if(lua_isnoneornil(L,3)) {
-        lua_pushliteral(L,"requires 3 parameters");
-        return lua_error(L);
-    }
-
-    h =  (const unsigned char *)lua_tolstring(L,1,&hlen);
-    in = (const unsigned char *)lua_tolstring(L,2,&inlen);
-    k =  (const unsigned char *)lua_tolstring(L,3,&klen);
-
-    if(hlen != crypto_auth_hmacsha512_BYTES) {
-        return luaL_error(L,"wrong tag size, expected: %d", crypto_auth_hmacsha512_BYTES);
-    }
-
-    if(klen != crypto_auth_hmacsha512_KEYBYTES) {
-        return luaL_error(L,"wrong key size, expected: %d", crypto_auth_hmacsha512_KEYBYTES);
-    }
-
-    lua_pushboolean(L,crypto_auth_hmacsha512_verify(h,in,inlen,k) == 0);
-    return 1;
-}
-
-static int
-ls_crypto_auth_hmacsha512_keygen(lua_State *L) {
-    unsigned char k[crypto_auth_hmacsha512_KEYBYTES];
-    crypto_auth_hmacsha512_keygen(k);
-    lua_pushlstring(L,(const char *)k,crypto_auth_hmacsha512_KEYBYTES);
-    sodium_memzero(k,crypto_auth_hmacsha512_KEYBYTES);
-    return 1;
-}
-
-static const struct luaL_Reg ls_crypto_auth_functions[] = {
-    LS_LUA_FUNC(crypto_auth),
-    LS_LUA_FUNC(crypto_auth_verify),
-    LS_LUA_FUNC(crypto_auth_keygen),
-    LS_LUA_FUNC(crypto_auth_hmacsha256),
-    LS_LUA_FUNC(crypto_auth_hmacsha256_verify),
-    LS_LUA_FUNC(crypto_auth_hmacsha256_keygen),
-    LS_LUA_FUNC(crypto_auth_hmacsha512256),
-    LS_LUA_FUNC(crypto_auth_hmacsha512256_verify),
-    LS_LUA_FUNC(crypto_auth_hmacsha512256_keygen),
-    LS_LUA_FUNC(crypto_auth_hmacsha512),
-    LS_LUA_FUNC(crypto_auth_hmacsha512_verify),
-    LS_LUA_FUNC(crypto_auth_hmacsha512_keygen),
-    { NULL, NULL },
-};
 
 LS_PUBLIC
 int luaopen_luasodium_crypto_auth_core(lua_State *L) {
@@ -315,7 +163,22 @@ int luaopen_luasodium_crypto_auth_core(lua_State *L) {
     lua_newtable(L);
 
     ls_lua_set_constants(L,ls_crypto_auth_constants,lua_gettop(L));
-    ls_lua_set_functions(L,ls_crypto_auth_functions,0);
+
+    LS_PUSH_CRYPTO_AUTH(crypto_auth, crypto_auth);
+    LS_PUSH_CRYPTO_AUTH_VERIFY(crypto_auth, crypto_auth);
+    LS_PUSH_CRYPTO_AUTH_KEYGEN(crypto_auth, crypto_auth);
+
+    LS_PUSH_CRYPTO_AUTH(crypto_auth_hmacsha256, crypto_auth);
+    LS_PUSH_CRYPTO_AUTH_VERIFY(crypto_auth_hmacsha256, crypto_auth);
+    LS_PUSH_CRYPTO_AUTH_KEYGEN(crypto_auth_hmacsha256, crypto_auth);
+
+    LS_PUSH_CRYPTO_AUTH(crypto_auth_hmacsha512256, crypto_auth);
+    LS_PUSH_CRYPTO_AUTH_VERIFY(crypto_auth_hmacsha512256, crypto_auth);
+    LS_PUSH_CRYPTO_AUTH_KEYGEN(crypto_auth_hmacsha512256, crypto_auth);
+
+    LS_PUSH_CRYPTO_AUTH(crypto_auth_hmacsha512, crypto_auth);
+    LS_PUSH_CRYPTO_AUTH_VERIFY(crypto_auth_hmacsha512, crypto_auth);
+    LS_PUSH_CRYPTO_AUTH_KEYGEN(crypto_auth_hmacsha512, crypto_auth);
 
     return 1;
 }
