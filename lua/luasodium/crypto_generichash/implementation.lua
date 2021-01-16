fd4ff4d219a1dbd249366cfca897be6bfb267061
John Regan <john@jrjrtech.com>
2021-01-16 09:18:18 -0500

    crypto_generichash: finish

--- lua/luasodium/crypto_generichash/implementation.lua
+++ lua/luasodium/crypto_generichash/implementation.lua
@@ -13,6 +13,9 @@ return function(libs, constants)
   local function ls_crypto_generichash(basename)
     local crypto_generichash_keygen = string_format('%s_keygen',basename)
     local crypto_generichash = string_format('%s',basename)
+    local crypto_generichash_init = string_format('%s_init',basename)
+    local crypto_generichash_update = string_format('%s_update',basename)
+    local crypto_generichash_final = string_format('%s_final',basename)
 
     local KEYBYTES = constants[string_format('%s_KEYBYTES',basename)]
     local KEYBYTES_MIN = constants[string_format('%s_KEYBYTES_MIN',basename)]
@@ -21,6 +24,18 @@ return function(libs, constants)
     local BYTES = constants[string_format('%s_KEYBYTES',basename)]
     local BYTES_MIN = constants[string_format('%s_KEYBYTES_MIN',basename)]
     local BYTES_MAX = constants[string_format('%s_KEYBYTES_MAX',basename)]
+
+    local STATEBYTES = sodium_lib[string_format('%s_statebytes',basename)]()
+
+    local ls_crypto_generichash_free = function(state)
+      sodium_lib.sodium_memzero(state,STATEBYTES)
+      clib.free(state)
+    end
+
+    local ls_crypto_generichash_methods = {}
+    local ls_crypto_generichash_mt = {
+      __index = ls_crypto_generichash_methods
+    }
 
     local M = {
       [crypto_generichash_keygen] = function()
@@ -73,7 +88,87 @@ return function(libs, constants)
         sodium_lib.sodium_memzero(out,outlen)
         return out_str
       end,
+
+      [crypto_generichash_init] = function(key, outlen)
+        local keylen = 0
+        if key then
+          keylen = string_len(key)
+          if keylen < KEYBYTES_MIN then
+            return error(string_format(
+              'key too small, required minimum: %d', KEYBYTES_MIN))
+          elseif keylen > KEYBYTES_MAX then
+            return error(string_format(
+              'key too large, required maximum: %d', KEYBYTES_MAX))
+          end
+        end
+
+        if outlen then
+          if outlen < BYTES_MIN then
+            return error(string_format(
+              'hash too small, required minimum: %d', BYTES_MIN))
+          elseif outlen > BYTES_MAX then
+            return error(string_format(
+              'hash too large, required maximum: %d', BYTES_MAX))
+          end
+        else
+          outlen = BYTES
+        end
+
+        local state = ffi.gc(clib.malloc(STATEBYTES),ls_crypto_generichash_free)
+
+        if tonumber(sodium_lib[crypto_generichash_init](state,key,keylen,outlen)) == -1 then
+          return error(string_format('%s error',crypto_generichash_init))
+        end
+
+        return setmetatable({
+          state = state,
+          outlen = outlen,
+        }, ls_crypto_generichash_mt)
+      end,
+
+      [crypto_generichash_update] = function(ls_state, message)
+        if not message then
+          return error('requires 2 parameters')
+        end
+
+        if getmetatable(ls_state) ~= ls_crypto_generichash_mt then
+          return error('invalid userdata')
+        end
+
+        local mlen = string_len(message)
+
+        return tonumber(sodium_lib[crypto_generichash_update](
+          ls_state.state, message, mlen)) == 0
+      end,
+
+      [crypto_generichash_final] = function(ls_state, outlen)
+        if not ls_state then
+          return error('requires 1 parameter')
+        end
+
+        if getmetatable(ls_state) ~= ls_crypto_generichash_mt then
+          return error('invalid userdata')
+        end
+
+        if not outlen then
+          outlen = ls_state.outlen
+        end
+
+        local out = char_array(outlen)
+
+        if tonumber(sodium_lib[crypto_generichash_final](ls_state.state,
+          out,outlen)) == -1 then
+          return error(string_format('%s error',crypto_generichash_final))
+        end
+
+        local out_str = ffi_string(out,outlen)
+        sodium_lib.sodium_memzero(out,outlen)
+        return out_str
+      end,
     }
+
+    ls_crypto_generichash_methods.update = M[crypto_generichash_update]
+    ls_crypto_generichash_methods.final = M[crypto_generichash_final]
 
     return M
   end
--- lua/luasodium/crypto_generichash/signatures.lua
+++ lua/luasodium/crypto_generichash/signatures.lua
@@ -13,10 +13,33 @@ local crypto_generichash_sig = [[
   const unsigned char *, size_t)
 ]]
 
+local crypto_generichash_init_sig = [[
+  int (%s)(
+  void *,
+  const unsigned char *,
+  const size_t, const size_t)
+]]
+
+local crypto_generichash_update_sig = [[
+  int (%s)(
+  void *,
+  const unsigned char *,
+  unsigned long long)
+]]
+
+local crypto_generichash_final_sig = [[
+  int (%s)(
+  void *,
+  unsigned char *, const size_t)
+]]
+
 local signatures = {
   ['crypto_generichash_keygen'] = crypto_generichash_keygen_sig,
   ['crypto_generichash_statebytes'] = crypto_generichash_statebytes_sig,
   ['crypto_generichash'] = crypto_generichash_sig,
+  ['crypto_generichash_init'] = crypto_generichash_init_sig,
+  ['crypto_generichash_update'] = crypto_generichash_update_sig,
+  ['crypto_generichash_final'] = crypto_generichash_final_sig,
 }
 
 return signatures
