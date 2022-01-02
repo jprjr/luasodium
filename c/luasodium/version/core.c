c3aafccab615d510f4ba549c3da82dfb944d3495
John Regan <john@jrjrtech.com>
2022-01-02 12:58:58 -0500

    Make the C-API 100% standalone
    This introduces some small duplication (the _VERSION* fields
    have to be defined in two places now), but it allows using
    luasodium with luastatic, without requiring any of the .lua
    modules.

--- c/luasodium/version/core.c
+++ c/luasodium/version/core.c
@@ -3,6 +3,7 @@
 #include "../internals/ls_lua_set_constants.h"
 #include "constants.h"
 
+
 static int
 ls_sodium_version_string(lua_State *L) {
     lua_pushstring(L,sodium_version_string());
@@ -37,20 +38,15 @@ static const struct luaL_Reg ls_version_functions[] = {
 
 LS_PUBLIC
 int luaopen_luasodium_version_core(lua_State *L) {
-    /* no need for newtable since requiring will place
-     * a table on top of the stack */
-    lua_getglobal(L,"require");
-    lua_pushliteral(L,"luasodium.version.implementation");
-    if(lua_pcall(L,1,1,0)) {
-        return lua_error(L);
-    }
-
-    if(lua_pcall(L,0,1,0)) {
-        return lua_error(L);
-    }
+    /* LCOV_EXCL_START */
+    LUASODIUM_INIT(L)
+    /* LCOV_EXCL_STOP */
+    lua_newtable(L);
 
     ls_lua_set_constants(L,ls_version_constants,lua_gettop(L));
     ls_lua_set_functions(L,ls_version_functions,0);
+    lua_pushliteral(L,LUASODIUM_VERSION);
+    lua_setfield(L,-2,"_VERSION");
 
     return 1;
 }
