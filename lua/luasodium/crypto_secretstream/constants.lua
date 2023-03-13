fd357b06394a54ec75b1975d7e15e8461ac28497
John Regan <john@jrjrtech.com>
2023-03-13 18:29:10 -0400

    crypto_secretstream: fix tag types in ffi mode

--- lua/luasodium/crypto_secretstream/constants.lua
+++ lua/luasodium/crypto_secretstream/constants.lua
@@ -3,10 +3,10 @@ local constant_keys = {
   { ['type'] = 1, ['name'] = 'crypto_secretstream_xchacha20poly1305_HEADERBYTES' },
   { ['type'] = 1, ['name'] = 'crypto_secretstream_xchacha20poly1305_KEYBYTES' },
   { ['type'] = 1, ['name'] = 'crypto_secretstream_xchacha20poly1305_MESSAGEBYTES_MAX' },
-  { ['type'] = 1, ['name'] = 'crypto_secretstream_xchacha20poly1305_TAG_MESSAGE' },
-  { ['type'] = 1, ['name'] = 'crypto_secretstream_xchacha20poly1305_TAG_PUSH' },
-  { ['type'] = 1, ['name'] = 'crypto_secretstream_xchacha20poly1305_TAG_REKEY' },
-  { ['type'] = 1, ['name'] = 'crypto_secretstream_xchacha20poly1305_TAG_FINAL' },
+  { ['type'] = 3, ['name'] = 'crypto_secretstream_xchacha20poly1305_TAG_MESSAGE' },
+  { ['type'] = 3, ['name'] = 'crypto_secretstream_xchacha20poly1305_TAG_PUSH' },
+  { ['type'] = 3, ['name'] = 'crypto_secretstream_xchacha20poly1305_TAG_REKEY' },
+  { ['type'] = 3, ['name'] = 'crypto_secretstream_xchacha20poly1305_TAG_FINAL' },
 }
 
 return constant_keys
